debb4eea77f0fbc0582b50f56edd6410e2765328
Neocrison96 <cristianpedraza96@gmail.com>
2024-06-04 18:59:36 -0500

    Adding some images, styles nad transitions

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.section');
+    
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+            }
+        });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+
     const contactForm = document.getElementById('contact-form');
     
     contactForm.addEventListener('submit', (e) => {
